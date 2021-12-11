0cd2bf2b1fa3f58e06c4b78b2a1e664e9d1b9a1b
adi <adrian.tworek.it@gmail.com>
2021-12-11 17:22:49 +0100

    Correct variable name

---
+++ types/tag.type.ts
@@ -0,0 +1,26 @@
+export type Tag =
+  | 'react'
+  | 'js-library'
+  | 'typescript'
+  | 'javascript'
+  | 'java'
+  | 'python'
+  | 'c++'
+  | 'go'
+  | 'swift'
+  | 'c#'
+  | 'php'
+
+export const tagFilters: Tag[] = [
+  'react',
+  'js-library',
+  'typescript',
+  'javascript',
+  'java',
+  'python',
+  'c++',
+  'go',
+  'swift',
+  'c#',
+  'php',
+]
