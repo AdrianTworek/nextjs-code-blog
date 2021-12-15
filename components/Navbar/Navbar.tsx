91cc98e5ee61f0880d0537d187270998620cfc8a
adi <adrian.tworek.it@gmail.com>
2021-12-15 19:48:48 +0100

    Implement posts filtering functionality

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 import { FC, useState, MouseEvent, useEffect } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
+import { Post } from '../../types/post.interface'
 
 import { useThemeContext } from '../../context/ThemeContext'
+import { usePostsContext } from '../../context/PostsContext'
 
 import { getTheme } from '../../utils/theme'
 
@@ -12,24 +16,41 @@ import {
   Button,
   Divider,
   IconButton,
+  InputLabel,
   Toolbar,
   Menu,
   MenuItem,
   Typography,
   Switch,
   useTheme,
+  Input,
+  FormControl,
+  InputAdornment,
+  Grid,
 } from '@mui/material'
 
+import SearchIcon from '@mui/icons-material/Search'
 import ComputerIcon from '@mui/icons-material/Computer'
 import MenuIcon from '@mui/icons-material/Menu'
 
+import FilteredBlogCard from './FilteredBlogCard'
+
 const Navbar: FC = () => {
-  const { state, dispatch, textColor } = useThemeContext()
+  const {
+    state: themeState,
+    dispatch: dispatchTheme,
+    textColor,
+  } = useThemeContext()
+  const { state: posts } = usePostsContext()
+
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([])
+  const [searchValue, setSearchValue] = useState<string>('')
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
   const [anchorMobileMenu, setAnchorMobileMenu] = useState<null | HTMLElement>(
     null
   )
 
+  const router = useRouter()
   const theme = useTheme()
   const menuItems = ['learn', 'blog']
 
@@ -38,12 +59,24 @@ const Navbar: FC = () => {
 
     if (theme) {
       if (theme === 'light') {
-        dispatch({ type: 'TOGGLE_LIGHT_MODE' })
+        dispatchTheme({ type: 'TOGGLE_LIGHT_MODE' })
       } else {
-        dispatch({ type: 'TOGGLE_DARK_MODE' })
+        dispatchTheme({ type: 'TOGGLE_DARK_MODE' })
       }
     }
   }, [])
+
+  // Reset filteredPosts state if there is nothing in input field
+  useEffect(() => {
+    if (!searchValue) {
+      setFilteredPosts([])
+    }
+  }, [searchValue])
+
+  // Reset filtered posts when changing the page
+  useEffect(() => {
+    resetFilteredPosts()
+  }, [router])
 
   const handleOpenMobileMenu = (event: MouseEvent<HTMLElement>) => {
     setAnchorMobileMenu(event.currentTarget)
@@ -58,137 +91,221 @@ const Navbar: FC = () => {
   }
 
   const handleThemeChange = () => {
-    if (state === 'dark') {
-      dispatch({ type: 'TOGGLE_LIGHT_MODE' })
+    if (themeState === 'dark') {
+      dispatchTheme({ type: 'TOGGLE_LIGHT_MODE' })
     } else {
-      dispatch({ type: 'TOGGLE_DARK_MODE' })
+      dispatchTheme({ type: 'TOGGLE_DARK_MODE' })
     }
   }
 
+  const searchBlogs = (input: string) => {
+    const filteredPosts = posts.filter((post: Post) =>
+      post.metaData.title.toLowerCase().includes(input)
+    )
+
+    setFilteredPosts(filteredPosts)
+  }
+
+  const resetFilteredPosts = () => {
+    setFilteredPosts([])
+    setSearchValue('')
+  }
+
   return (
-    <AppBar
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: 70,
-        background: theme.palette.background.paper,
-        color: theme.palette.text.primary,
-      }}
-      position="static"
-    >
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Typography
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '0.75rem',
-              fontWeight: 700,
-              fontSize: '1.2rem',
-              '@media screen and (min-width: 37.5em)': {
-                fontSize: '1.6rem',
-              },
-            }}
-            variant="h6"
-            noWrap
-            component="div"
-          >
-            <ComputerIcon fontSize="medium" />
-            Code Blog
-          </Typography>
+    <>
+      <AppBar
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: 75,
+          background: theme.palette.background.paper,
+          color: theme.palette.text.primary,
+        }}
+        position="static"
+      >
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Link href="/" passHref>
+              <Typography
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '0.75rem',
+                  fontWeight: 700,
+                  fontSize: '1.2rem',
+                  cursor: 'pointer',
+                  '@media screen and (min-width: 37.5em)': {
+                    fontSize: '1.6rem',
+                  },
+                }}
+                variant="h6"
+                noWrap
+                component="div"
+              >
+                <ComputerIcon fontSize="medium" />
+                Code Blog
+              </Typography>
+            </Link>
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: 'none', sm: 'flex' },
-              gap: '0.5rem',
-              marginLeft: '2.5rem',
-            }}
-          >
-            {menuItems.map((link) => (
-              <Link key={link} href={`/${link}`} passHref>
-                <Button
-                  sx={{
-                    fontSize: '0.9rem',
-                    transitionDuration: '0s',
-                  }}
-                  color={textColor}
-                >
-                  {link}
-                </Button>
-              </Link>
-            ))}
-          </Box>
-
-          <Box
-            sx={{
-              display: 'none',
-              marginLeft: 'auto',
-              '@media screen and (min-width: 37.5em)': { display: 'block' },
-            }}
-          >
-            <Switch
-              checked={state === 'dark'}
-              color="primary"
-              onChange={handleThemeChange}
-            />
-          </Box>
-
-          <Box
-            sx={{
-              display: 'flex',
-              marginLeft: 'auto',
-              '@media screen and (min-width: 37.5em)': { display: 'none' },
-            }}
-          >
-            <IconButton color={textColor} onClick={handleOpenMobileMenu}>
-              <MenuIcon fontSize="large" color={textColor} />
-            </IconButton>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorMobileMenu}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', sm: 'flex' },
+                gap: '0.5rem',
+                marginLeft: '2.5rem',
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={!!anchorMobileMenu}
-              onClose={handleCloseUserMenu}
             >
-              {menuItems.map((item) => (
-                <Link key={item} href={`/${item}`} passHref>
-                  <MenuItem key={item} onClick={handleCloseMobileMenu}>
-                    {item.charAt(0).toUpperCase() + item.slice(1)}
-                  </MenuItem>
+              {menuItems.map((link) => (
+                <Link key={link} href={`/${link}`} passHref>
+                  <Button
+                    sx={{
+                      fontSize: '0.9rem',
+                      transitionDuration: '0s',
+                    }}
+                    color={textColor}
+                  >
+                    {link}
+                  </Button>
                 </Link>
               ))}
+            </Box>
 
-              <Divider sx={{ background: 'rgba(255, 255, 255, 0.3)' }} />
+            {/* Show filtering form if the user has already visited /blog 
+            and data is in the PostsContext state */}
+            {posts.length > 0 && router.pathname !== '/blog' && (
+              <Box
+                sx={{
+                  width: 85,
+                  marginLeft: '1rem',
+                  '@media screen and (min-width: 23.4375em)': { width: 120 },
+                  '@media screen and (min-width: 37.5em)': {
+                    marginRight: '1.5rem',
+                  },
+                  '@media screen and (min-width: 48em)': { width: 200 },
+                }}
+                component={motion.div}
+                initial={{ x: -15, scale: 1, opacity: 0 }}
+                animate={{ x: 0, scale: [1, 1.1, 0.9, 1], opacity: 1 }}
+              >
+                <FormControl variant="standard" color={textColor}>
+                  <InputLabel htmlFor="blog-search-input">
+                    Search blogs
+                  </InputLabel>
+                  <Input
+                    id="blog-search-input"
+                    value={searchValue}
+                    onChange={(e) => {
+                      const input = e.target.value
+                      setSearchValue(input)
+                      searchBlogs(input.toLowerCase())
+                    }}
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <InputLabel htmlFor="blog-search-input">
+                          <SearchIcon color={textColor} />
+                        </InputLabel>
+                      </InputAdornment>
+                    }
+                  ></Input>
+                </FormControl>
+              </Box>
+            )}
 
-              <MenuItem>
-                <Box>
-                  <Switch
-                    checked={state === 'dark'}
-                    color="primary"
-                    onChange={handleThemeChange}
-                  />
-                </Box>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Box
+              sx={{
+                display: 'none',
+                marginLeft: 'auto',
+                '@media screen and (min-width: 37.5em)': { display: 'block' },
+              }}
+            >
+              <Switch
+                checked={themeState === 'dark'}
+                color="primary"
+                onChange={handleThemeChange}
+              />
+            </Box>
+
+            <Box
+              sx={{
+                display: 'flex',
+                marginLeft: 'auto',
+                '@media screen and (min-width: 37.5em)': { display: 'none' },
+              }}
+            >
+              <IconButton color={textColor} onClick={handleOpenMobileMenu}>
+                <MenuIcon fontSize="large" color={textColor} />
+              </IconButton>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorMobileMenu}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={!!anchorMobileMenu}
+                onClose={handleCloseUserMenu}
+              >
+                {menuItems.map((item) => (
+                  <Link key={item} href={`/${item}`} passHref>
+                    <MenuItem key={item} onClick={handleCloseMobileMenu}>
+                      {item.charAt(0).toUpperCase() + item.slice(1)}
+                    </MenuItem>
+                  </Link>
+                ))}
+
+                <Divider sx={{ background: 'rgba(255, 255, 255, 0.3)' }} />
+
+                <MenuItem>
+                  <Box>
+                    <Switch
+                      checked={themeState === 'dark'}
+                      color="primary"
+                      onChange={handleThemeChange}
+                    />
+                  </Box>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      {filteredPosts.length > 0 && (
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            padding: '1rem',
+            background: 'rgba(14, 14, 14, 0.5)',
+          }}
+          component={motion.div}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.25, ease: 'easeInOut' },
+          }}
+        >
+          {filteredPosts.map((post: Post) => (
+            <Grid key={post.slug} item>
+              <FilteredBlogCard post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </>
   )
 }
 
